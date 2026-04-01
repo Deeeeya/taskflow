8f93c6165ba8cc4b8aa28304c981a3bbd461778d
Deeeeya <tankonated@gmail.com>
2026-04-01 01:12:01 -0400

    feat: initialize client and server setup

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import type { Request, Response } from "express"
+
+const app = express(); // creates server instance
+const PORT = process.env.PORT||3000 // use the env's port if it exists, otherwise use 3000
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Server is running!' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on the port! ${PORT}`)
+})
